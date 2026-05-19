59a42cd47df48f22acdf2845c58b80baa7259d86
Wojciech Lesicki <wojciech.lesicki@gmail.com>
2026-05-19 08:27:36 +0200

    chore: remove payloads, migrated to external repo ide-test-payloads

--- payloads/linux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "[MOCK MALWARE] This could be something malicious running on Linux/macOS!"
-ps aux | head -n 5
-exit 0
--- payloads/windows.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo "[MOCK MALWARE] This could be something malicious running on Windows!"
-powershell -Command "Get-Process | Select-Object -First 5"
-exit /b 0
